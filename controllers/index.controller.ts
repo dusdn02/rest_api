eeec5883ff225e3a261fa19d979bb95406ad2b20
dusdn02 <lyw9114@naver.com>
2021-03-17 10:15:47 +0900

    restAPI 초기 세팅

--- controllers/index.controller.ts
+++
@@ -1,6 +0,0 @@
-const index = (_req: any, res: any, _next: any) =>
-  res.json({
-    message: 'Hello Aha!'
-  })
-
-export { index }
--- routes/v1/kafka.route.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { send, messages } from '../../controllers/v1/kafka.controller'
-
-const router = express.Router()
-
-router.route('/send').get(send)
-router.route('/messages').get(messages)
-
-export default router
